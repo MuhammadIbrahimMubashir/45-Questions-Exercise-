79b6be3b21e3795f87e51298e85b4da262b4472b
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2024-03-06 22:58:51 +0500

    Add files via upload

---
+++ 14.ts
@@ -0,0 +1,4 @@
+let guests: string[] = ["Albert Einstein", "Marie Curie", "Nikola Tesla"];
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
---
+++ 15.ts
@@ -0,0 +1,11 @@
+let unableToAttend = "Nikola Tesla";
+console.log(`${unableToAttend} can't make it to dinner.`);
+
+// Replace the guest
+let newGuest = "Leonardo da Vinci";
+guests[guests.indexOf(unableToAttend)] = newGuest;
+
+// New invitations
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
