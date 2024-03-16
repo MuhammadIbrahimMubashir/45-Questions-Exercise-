5dbbcdc9131aa07bc1967e20af7076508cb6f9a9
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2024-03-16 13:18:50 +0500

    Delete 5.js

--- 5.js
+++
@@ -1,3 +0,0 @@
-var famous_person = "Albert Einstein";
-var message = "".concat(famous_person, " once said, \u201CA person who never made a mistake never tried anything new.\u201D");
-console.log(message);
