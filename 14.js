56992449b13edd24a8541f500e25431d03377a47
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2024-03-16 13:17:52 +0500

    Delete 14.js

--- 14.js
+++
@@ -1,4 +0,0 @@
-var guests = ["Albert Einstein", "Marie Curie", "Nikola Tesla"];
-guests.forEach(function (guest) {
-    console.log("Dear ".concat(guest, ", would you like to join me for dinner?"));
-});
