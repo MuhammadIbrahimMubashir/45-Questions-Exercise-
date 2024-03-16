ba1ab82b7466deb8b47d7149efe06a21f90087ec
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2024-03-16 13:15:50 +0500

    Delete 3.ts

--- 3.ts
+++
@@ -1,4 +0,0 @@
-let name: string = "Muhammad Ibrahim Mubashir"; 
-console.log(name.toLowerCase()); 
-console.log(name.toUpperCase()); 
-console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
